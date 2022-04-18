3dddcacf2f66b4b0773f15c023cd4b282e278480
remiCzn <cazinremi2@gmail.com>
2022-04-18 17:49:45 +0200

    Init repo

---
+++ app-chat-web/src/app/components/login/login.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+})
+export class LoginComponent implements OnInit {
+  public password: string = '';
+  public email: string = '';
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
