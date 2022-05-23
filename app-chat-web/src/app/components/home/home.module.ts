bddd18fdeb4a5ab1d41f0e1cc84190a3d937d3b9
remiCzn <cazinremi2@gmail.com>
2022-05-23 21:31:14 +0200

    Format Chat

--- app-chat-web/src/app/components/home/home.module.ts
+++ app-chat-web/src/app/components/home/home.module.ts
@@ -1,24 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent } from './header/header.component';
-import { RightHeaderComponent } from './header/right-header/right-header.component';
-import { CenterHeaderComponent } from './header/center-header/center-header.component';
-import { LeftHeaderComponent } from './header/left-header/left-header.component';
 import { HomeComponent } from './home.component';
+import { ServersListComponent } from './servers-list/servers-list.component';
+import { ChannelsComponent } from './channels/channels.component';
 import { MainComponent } from './main/main.component';
-import { ChannelsComponent } from './main/channels/channels.component';
-import { ContentComponent } from './main/content/content.component';
+import { ServerButtonComponent } from './servers-list/server-button/server-button.component';
 
 @NgModule({
   declarations: [
-    HeaderComponent,
-    RightHeaderComponent,
-    CenterHeaderComponent,
-    LeftHeaderComponent,
     HomeComponent,
-    MainComponent,
+    ServersListComponent,
     ChannelsComponent,
-    ContentComponent,
+    MainComponent,
+    ServerButtonComponent,
   ],
   imports: [CommonModule],
 })
---
+++ app-chat-web/src/app/components/home/servers-list/server-button/server-button.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-server-button',
+  templateUrl: './server-button.component.html',
+  styleUrls: ['./server-button.component.css']
+})
+export class ServerButtonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
